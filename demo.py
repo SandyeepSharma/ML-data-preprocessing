dfc45fc611328ad4db1bb9838293506b19843edc
Sandeep Sharma <50715831+SandyeepSharma@users.noreply.github.com>
2023-11-08 10:30:52 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  6 20:11:20 2019
 
-@author: sandy
-"""
-
-import pandas as pd
-data = pd.read_csv(r"C:\Users\sandy\Dropbox\Part 0 - Self_practice\titanic\dataset\train.csv")
-data.head(5)
-data.info()
-sum(data.isnull())
